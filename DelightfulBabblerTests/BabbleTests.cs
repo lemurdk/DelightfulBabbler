743361b1a8284df1deb956ff4e599564d1756651
Emil Rossing <emil@lemur.dk>
2020-12-02 19:14:37 +0100

    Added code

---
+++ DelightfulBabblerTests/BabbleTests.cs
@@ -0,0 +1,17 @@
+﻿using DelightfulBabbler;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DelightfulBabblerTests {
+   [TestClass]
+   public class BabbleTests {
+      [TestMethod()]
+      public void RandomTest()
+      {
+         var x = Babble.Random();
+         Assert.IsNotNull(x);
+         var y = Babble.Random();
+         Assert.IsNotNull(y);
+         Assert.AreNotEqual(x,y);
+      }
+   }
+}
