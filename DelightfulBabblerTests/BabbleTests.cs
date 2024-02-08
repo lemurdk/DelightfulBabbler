6c7cd609989fdb53f3dfd9bd14200803a0dc5148
Emil Rossing <emil@rossing.org>
2024-02-08 08:22:10 +0100

    File namespace

--- DelightfulBabblerTests/BabbleTests.cs
+++ DelightfulBabblerTests/BabbleTests.cs
@@ -1,17 +1,16 @@
 ﻿using DelightfulBabbler;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace DelightfulBabblerTests {
-   [TestClass]
-   public class BabbleTests {
-      [TestMethod()]
-      public void RandomTest()
-      {
-         var x = Babble.Random();
-         Assert.IsNotNull(x);
-         var y = Babble.Random();
-         Assert.IsNotNull(y);
-         Assert.AreNotEqual(x,y);
-      }
-   }
-}
+namespace DelightfulBabblerTests; 
+ [TestClass]
+ public class BabbleTests {
+    [TestMethod()]
+    public void RandomTest()
+    {
+       var x = Babble.Random();
+       Assert.IsNotNull(x);
+       var y = Babble.Random();
+       Assert.IsNotNull(y);
+       Assert.AreNotEqual(x,y);
+    }
+ }
